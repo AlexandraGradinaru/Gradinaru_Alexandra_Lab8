5e63c8f3f32b5c3aa7d92728c7bd9f91983b04a2
ALEXANDRA-GABRIELA GRĂDINARU <alexandra.gradinaru@stud.ubbcluj.ro>
2020-11-26 09:22:17 +0200

    Add project files.

---
+++ Data/Gradinaru_Alexandra_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Gradinaru_Alexandra_Lab8.Models;
+
+namespace Gradinaru_Alexandra_Lab8.Data
+{
+    public class Gradinaru_Alexandra_Lab8Context : DbContext
+    {
+        public Gradinaru_Alexandra_Lab8Context (DbContextOptions<Gradinaru_Alexandra_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Gradinaru_Alexandra_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Gradinaru_Alexandra_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Gradinaru_Alexandra_Lab8.Models.Category> Category { get; set; }
+    }
+}
